cda2dee1fbead0c38afc642a0060204a8c903f8b
andrusrem <andrusremets@gmail.com>
2024-12-22 10:27:27 +0200

    Add language change to Button shared component

--- react/src/shared/Button.jsx
+++ react/src/shared/Button.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Button.css";
 
+import { useLanguage } from "../LanguageContext"; 
+
+
+// Translation for header content
+const translations = {
+    en: {
+      book: "Book",
+    },
+    et: {
+      book: "Broneeri",
+    },
+    ru: {
+      book: "Забронировать",
+    },
+    fin: {
+      book: "Varaa",
+  },
+  };
 const BookButton = () => {
+    const { language } = useLanguage();
     return (
         <div className="small-screen-button-container">
-            <button className="small-screen-button">Book</button>
+            <button className="small-screen-button">{translations[language].book}</button>
         </div>
         
     )
